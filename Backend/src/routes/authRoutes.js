d920a15c920d20a8401773ec4247609b621f2269
huyleo03 <giahuyn495@gmail.com>
2025-09-30 01:27:24 +0700

    xóa file auth

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -1,21 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controller/AuthController");
-const auth = require("../middlewares/auth");
+const authenticate = require("../middlewares/authMiddleware").authenticate;
 
 // Forgot password (gửi OTP)
 router.post("/forgot-password", authController.forgotPassword);
 
 // Verify OTP (chỉ cần resetToken + otp)
-router.post("/verify-otp", auth, authController.verifyOtp);
+router.post("/verify-otp", authenticate, authController.verifyOtp);
 
 // Reset password (cần resetToken trong headers)
-router.post("/reset-password", auth, authController.resetPassword);
+router.post("/reset-password", authenticate, authController.resetPassword);
 
 // Login
 router.post("/login", authController.login);
-
-
-
 
 module.exports = router;
