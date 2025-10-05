96df9d5ec7e05ee4c9678e5009eb226091c3dee4
Truong Anh Duc <ductahe170028@fpt.edu.vn>
2025-10-05 18:26:27 +0700

    request

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Request from "./pages/Request";
+import { Button, Box } from "@mui/material";
 
 function App() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      sx={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Button variant="contained" onClick={() => setOpen(true)}>
+        New Request
+      </Button>
+      <Request open={open} onClose={() => setOpen(false)} />
+    </Box>
   );
 }
 
